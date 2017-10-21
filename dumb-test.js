28eb9571df04cb55af68609833462398db8863fc
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2017-10-22 00:47:43 +0300

    Fixed a bug with promises in index file and improved dumb-test

--- dumb-test.js
+++ dumb-test.js
@@ -1,18 +1,22 @@
-const limiter = require('../lib');
+const createLimiter = require('./lib');
 
+const limit = createLimiter(10); // Will limit to 10 concurrent jobs
 let nrRunning = 0;
-const testFunc = () => {
+
+const myAsyncfunction = (value) => {
   nrRunning += 1;
-  console.log(nrRunning);
-  return new Promise(resolve => setTimeout(() => {
-    console.log(nrRunning);
-    nrRunning -= 1;
-    resolve();
-  }, Math.random() * 1000));
+  return new Promise(resolve =>
+    setTimeout(
+      () => {
+        nrRunning -= 1;
+        resolve(value);
+      },
+      Math.random() * 1000
+    ));
 };
 
-
-const limit = limiter(10);
-for (let i = 0; i < 100; i += 1) {
-  limit(testFunc)(i);
+const checkInterval = setInterval(() => console.log('Currently running', nrRunning, 'jobs...'), 100);
+setTimeout(() => clearInterval(checkInterval), 4000);
+for (let i = 0; i < 30; i += 1) {
+  limit(myAsyncfunction)(i).then(value => console.log('Resolved', value));
 }
--- src/index.js
+++ src/index.js
@@ -4,27 +4,36 @@ type InputFunction = (...args: any) => Promise<any>
 type WrapperFunction = (inputFunction: InputFunction) => InputFunction
 
 export default (limit: number): WrapperFunction => {
-  let running: Array<Promise<any>> = [];
+  // Array of currently running jobs
+  let runningJobs: Array<Promise<any>> = [];
 
   // Return a wrapper function
-  return (inputFunction: InputFunction): InputFunction => {
-    const callWrapper: InputFunction = (...args: any) => new Promise((resolve) => {
-      if (running.length < limit) {
-        // If we are under the limit, run this job right away
-        const currentPromise = inputFunction(...args).then((result) => {
-          // Remove this job from the list on completion
-          running = running.filter(item => item !== currentPromise);
-          resolve(result);
-        });
-        running.push(currentPromise);
-      } else {
-        // Run this function again when the first job completes to see if we can run now
-        Promise.race(running).then(() => {
-          callWrapper(...args);
-        });
-      }
-    });
+  return (inputFunction: InputFunction): InputFunction =>
+    // ...that returns a wrapped version of the function
+    (...args: any) =>
+      // ...that returns a wrapper promise
+      new Promise((resolve) => {
+        // Define the cheking part as a function so we can call it recursively
+        const checkQueue = () => {
+          if (runningJobs.length < limit) {
+            // If we are under the limit, run this job right away
+            const currentPromise = inputFunction(...args).then((result) => {
+              // Remove this job from the list on completion
+              runningJobs = runningJobs.filter(item => item !== currentPromise);
 
-    return callWrapper;
-  };
+              // Resolve the wrapper promise with correct result
+              resolve(result);
+            });
+            runningJobs.push(currentPromise);
+          } else {
+            // Run this function again whenever a job completes to check if our job can be started
+            Promise.race(runningJobs).then(() => {
+              checkQueue();
+            });
+          }
+        };
+
+        // Call the checker function
+        checkQueue();
+      });
 };
