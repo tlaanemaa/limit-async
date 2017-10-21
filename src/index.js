180680596eeb1d799e8cb9e66805a012792e6003
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2017-10-21 22:04:33 +0300

    Wrote the limiter code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-// @flow
+
+export default (limit) => {
+  let running = [];
+
+  // Return a wrapper function
+  return (inputFunction) => {
+    const callWrapper = (...args) => new Promise((resolve) => {
+      if (running.length < limit) {
+        // If we are under the limit, run this job right away
+        const currentPromise = inputFunction(...args).then((result) => {
+          // Remove this job from the list on completion
+          running = running.filter(item => item !== currentPromise);
+          resolve(result);
+        });
+        running.push(currentPromise);
+      } else {
+        // Run this function again when the first job completes to see if we can run now
+        Promise.race(running).then(() => {
+          callWrapper(...args);
+        });
+      }
+    });
+
+    return callWrapper;
+  };
+};
